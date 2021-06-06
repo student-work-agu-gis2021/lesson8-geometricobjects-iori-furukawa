e4053f820f0bba112eb320b22fb2ad0602ef814c
iorifurukawa <>
2021-06-06 16:44:58 +0000

    Define poly1.

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -84,7 +84,7 @@ return Polygon(coords)
 # Demonstrate the usage of the function. For example, create a Polygon with three points: `(45.2, 22.34)`, `(100.22, -3.20)` & `(70.0, 10.20)`.
 
 # YOUR CODE HERE 5 to define poly1 with three points
-poly1 = 
+poly1 = LineString([(45.2,22.34),(100.22,-3.20),(70.0,10.20)])
 
 # CODE FOR TESTING YOUR SOLUTION
 print(poly1)
